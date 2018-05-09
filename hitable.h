e1aaffd8b5787c4dfb10fdb6469a41e5ff8dd52d
Roger Allen <rallen@gmail.com>
2018-05-08 19:30:39 -0700

    Chapter 5 done.

--- hitable.h
+++ hitable.h
@@ -12,7 +12,7 @@ struct hit_record
 
 class hitable  {
     public:
-        virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const = 0;
+        __device__ virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const = 0;
 };
 
 #endif
--- main.cu
+++ main.cu
@@ -1,42 +1,116 @@
 #include <iostream>
+#include <time.h>
+#include <float.h>
+#include "vec3.h"
+#include "ray.h"
 #include "sphere.h"
 #include "hitable_list.h"
-#include "float.h"
 
-vec3 color(const ray& r, hitable *world) {
-    hit_record rec;
-    if (world->hit(r, 0.0, MAXFLOAT, rec)) {
-        return 0.5*vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
-    }
-    else {
-        vec3 unit_direction = unit_vector(r.direction());
-        float t = 0.5*(unit_direction.y() + 1.0);
-        return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+// limited version of checkCudaErrors from helper_cuda.h in CUDA examples
+#define checkCudaErrors(val) check_cuda( (val), #val, __FILE__, __LINE__ )
+
+void check_cuda(cudaError_t result, char const *const func, const char *const file, int const line) {
+    if (result) {
+        std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << " at " <<
+            file << ":" << line << " '" << func << "' \n";
+        // Make sure we call CUDA Device Reset before exiting
+        cudaDeviceReset();
+        exit(99);
     }
 }
 
+__device__ vec3 color(const ray& r, hitable **world) {
+    hit_record rec;
+    if ((*world)->hit(r, 0.0, FLT_MAX, rec)) {
+        return 0.5f*vec3(rec.normal.x()+1.0f, rec.normal.y()+1.0f, rec.normal.z()+1.0f);
+    }
+    else {
+        vec3 unit_direction = unit_vector(r.direction());
+        float t = 0.5f*(unit_direction.y() + 1.0f);
+        return (1.0f-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+    }
+}
+
+__global__ void render(vec3 *fb, int max_x, int max_y,
+                       vec3 lower_left_corner, vec3 horizontal, vec3 vertical, vec3 origin,
+                       hitable **world) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    if((i >= max_x) || (j >= max_y)) return;
+    int pixel_index = j*max_x + i;
+    float u = float(i) / float(max_x);
+    float v = float(j) / float(max_y);
+    ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+    fb[pixel_index] = color(r, world);
+}
+
+__global__ void create_world(hitable **d_list, hitable **d_world) {
+    if (threadIdx.x == 0 && blockIdx.x == 0) {
+        *(d_list)   = new sphere(vec3(0,0,-1), 0.5);
+        *(d_list+1) = new sphere(vec3(0,-100.5,-1), 100);
+        *d_world    = new hitable_list(d_list,2);
+    }
+}
+
+__global__ void free_world(hitable **d_list, hitable **d_world) {
+    delete *d_world;
+    delete *d_list;
+}
+
 int main() {
-    int nx = 200;
-    int ny = 100;
+    int nx = 1440;
+    int ny = 720;
+    int tx = 8;
+    int ty = 8;
+
+    std::cerr << "Rendering a " << nx << "x" << ny << " image ";
+    std::cerr << "in " << tx << "x" << ty << " blocks.\n";
+
+    int num_pixels = nx*ny;
+    size_t fb_size = num_pixels*sizeof(vec3);
+
+    // allocate FB
+    vec3 *fb;
+    checkCudaErrors(cudaMallocManaged((void **)&fb, fb_size));
+
+    // make our world of hitables
+    hitable **d_list;
+    checkCudaErrors(cudaMalloc((void **)&d_list, 2*sizeof(hitable *)));
+    hitable **d_world;
+    checkCudaErrors(cudaMalloc((void **)&d_world, sizeof(hitable *)));
+    create_world<<<1,1>>>(d_list,d_world);
+    checkCudaErrors(cudaDeviceSynchronize());
+
+    clock_t start, stop;
+    start = clock();
+    // Render our buffer
+    dim3 blocks(nx/tx+1,ny/ty+1);
+    dim3 threads(tx,ty);
+    render<<<blocks, threads>>>(fb, nx, ny,
+                                vec3(-2.0, -1.0, -1.0),
+                                vec3(4.0, 0.0, 0.0),
+                                vec3(0.0, 2.0, 0.0),
+                                vec3(0.0, 0.0, 0.0),
+                                d_world);
+    checkCudaErrors(cudaDeviceSynchronize());
+    stop = clock();
+    double timer_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
+    std::cerr << "took " << timer_seconds << " seconds.\n";
+
+    // Output FB as Image
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-    vec3 lower_left_corner(-2.0, -1.0, -1.0);
-    vec3 horizontal(4.0, 0.0, 0.0);
-    vec3 vertial(0.0, 2.0, 0.0);
-    vec3 origin(0.0, 0.0, 0.0);
-    hitable *list[2];
-    list[0] = new sphere(vec3(0,0,-1), 0.5);
-    list[1] = new sphere(vec3(0,-100.5,-1), 100);
-    hitable *world = new hitable_list(list,2);
     for (int j = ny-1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-            float u = float(i) / float(nx);
-            float v = float(j) / float(ny);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertial);
-            vec3 col = color(r, world);
-            int ir = int(255.99*col[0]);
-            int ig = int(255.99*col[1]);
-            int ib = int(255.99*col[2]);
+            size_t pixel_index = j*nx + i;
+            int ir = int(255.99*fb[pixel_index].r());
+            int ig = int(255.99*fb[pixel_index].g());
+            int ib = int(255.99*fb[pixel_index].b());
             std::cout << ir << " " << ig << " " << ib << "\n";
         }
     }
+
+    // clean up
+    free_world<<<1,1>>>(d_list,d_world);
+    checkCudaErrors(cudaDeviceSynchronize());
+    checkCudaErrors(cudaFree(fb));
 }
